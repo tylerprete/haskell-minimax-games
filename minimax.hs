d47b06bed7ad586856b12886bb1a0306f738b071
psyonic <psyonic@gmail.com>
2008-04-06 22:17:49 +0000

    Changed minimax to use minimumBy or maximumBy, respectively.  Kindof pointless, as I don't really even use minimax anymore, but still much cleaner that way.

--- minimax.hs
+++ minimax.hs
@@ -1,36 +1,19 @@
 module Minimax
 where
 import Debug.Trace
+import Data.List
+import Data.Ord
 
 class GameState a where
-	evaluateState	:: a -> Int
+	evaluateState	:: a -> b
 	terminalState	:: a -> Bool
 	genSuccessors	:: a -> [Int]
 	makeSuccessor	:: a -> Int -> a
 
-{-minimax	::	a -> Bool -> (a -> Bool -> [a]) -> (a -> Int) -> (a -> Bool) -> Int
-minimax gameState _ _ evaluate terminal | terminal gameState = evaluate gameState
-minimax gameState minimize genSuccessors ev term =
-	minOrMax $ (parMap rwhnf) (\gs -> minimax gs (not minimize) genSuccessors ev term) (genSuccessors gameState minimize)
-	where minOrMax = if minimize then minimum else maximum -}
-
-maxFstPair	:: (Ord a) => (a,b) -> (a,b) -> (a,b)
-maxFstPair x@(a1,b1) (a2,b2) | a1 >= a2 = x
-maxFstPair _ y = y
-
-minFstPair	:: (Ord a) => (a,b) -> (a,b) -> (a,b)
-minFstPair x@(a1,b1) (a2,b2) | a1 <= a2 = x
-minFstPair _ y = y
-
-opFstPairList	:: (Ord a) => ((a,b) -> (a,b) -> (a,b)) -> [(a,b)] -> (a,b)
-opFstPairList op [] = error "Don't call this with empty list"
-opFstPairList op ((a,b):[]) = (a,b)
-opFstPairList op ((a,b):xs) = op (a,b) (opFstPairList op xs) 
-
 minimax	:: (GameState a) => a -> Bool -> Int -> Int -> (Int, Maybe Int)
 minimax gs _ depth depthlimit | depth == depthlimit || terminalState gs = (evaluateState gs, Nothing)
 minimax gs minimize depth depthlimit =
-	let	minOrMax = opFstPairList (if minimize then minFstPair else maxFstPair)
+	let	minOrMax = (if minimize then minimumBy else maximumBy) (comparing fst)
 		successors = (genSuccessors gs)
 		scores = map fst $ map (\succ -> (minimax (makeSuccessor gs succ) (not minimize) (depth+1) depthlimit)) successors
 		wrappedSuccessors = map Just successors
